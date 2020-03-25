dede30c72fe260f38d99c3d76c6e33140835f07f
Tom Swope <tjswope@gmail.com>
2020-03-25 12:25:17 -0400

    initial commit.

---
+++ Clock/Clock.swift
@@ -0,0 +1,15 @@
+//
+//  Clock.swift
+//  Clock
+//
+//  Created by Swope, Thomas on 10/11/18.
+//  Copyright © 2018 Swope, Thomas. All rights reserved.
+//
+
+import UIKit
+
+class Clock{
+    
+    var currentTime: Date { return Date() }
+    
+}
--- Clock/ViewController.swift
+++ Clock/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    let clock = Clock()
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateTimeLabel()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeLabel), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimeLabel(){
+        let formatter = DateFormatter()
+        formatter.timeStyle = .medium
+        timeLabel.text = formatter.string(from: clock.currentTime)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    deinit{
+        if let timer = self.timer{
+            timer.invalidate()
+        }
+    }
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
